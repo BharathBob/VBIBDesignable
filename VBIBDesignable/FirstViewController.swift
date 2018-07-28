a309b239aed98fc5860eeca884723b61596c4b63
Viswa Bharath <viswa.bharath@docsapp.in>
2018-07-28 17:05:48 +0530

    Initial Commit

---
+++ VBIBDesignable/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  VBIBDesignable
+//
+//  Created by Vishwa Bharath on 28/07/18.
+//  Copyright © 2018 ViswaBharathD. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
